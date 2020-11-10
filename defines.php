b16b80f8f294b58c4ac3ff8886eb5d62c8a1e873
yotsu <yotsu@ubuntu-dev.home>
2020-11-10 15:02:40 +0100

    commit

--- defines.php
+++ defines.php
@@ -1,16 +1,16 @@
 <?php
 	session_start();
 
-	define( 'FACEBOOK_APP_ID', 'YOUR-APP-ID' );
-	define( 'FACEBOOK_APP_SECRET', 'YOUR-APP-SECRED' );
-	define( 'FACEBOOK_REDIRECT_URI', 'YOUR-REDIRECT-URI' );
+	define( 'FACEBOOK_APP_ID', '765905817297296' );
+	define( 'FACEBOOK_APP_SECRET', '33a5779236d1be1a8ace3435331f7e45' );
+	define( 'FACEBOOK_REDIRECT_URI', 'https://seazonal.fr' );
 	define( 'ENDPOINT_BASE', 'https://graph.facebook.com/v5.0/' );
 
 	// accessToken
-	$accessToken = 'YOUR-ACCESS-TOKEN-HERE';
+	$accessToken = 'EAAK4llgr4ZAABAJ4Bud1SUZC2HwxqS57TDtE8tVzmwSIdxZBPMx8wOv93Lue0FZAaeYc5NNuKFkemQCrmJ3iORMI458jKXCWAX0s0yBZBtLg9twLJfBMviZCWt0EEUwOCARTQBkYZCJwzKsudtbTRtxhZCokXxxG5a4C0zR6kdyhQMiPVm6iN0DVPZAWBZAHo3XCgMFtz1PFzf0AZDZD';
 
 	// page id
-	$pageId = 'YOUR-PAGE-ID';
+	$pageId = '106433811280272';
 
 	// instagram business account id
-	$instagramAccountId = 'YOUR-INSTAGRAM-ACCOUNT-ID';
+	$instagramAccountId = '42798197610';
